5842fc4770ce0a2e0b0cfc4ede8b4e864677be74
Erik Dorstel <43003063+ErikDorstel@users.noreply.github.com>
2021-02-11 07:24:04 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+curl https://raw.githubusercontent.com/ErikDorstel/tasView/main/index.html > index.html
+curl https://raw.githubusercontent.com/hivemq/hivemq-mqtt-web-client/master/js/mqttws31.js > mqttws31.js
